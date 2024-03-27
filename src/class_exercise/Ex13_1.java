3a9938388ddfae866c3f51dbd75e2e7e70035202
Sophia <sophia@Sophiade-MacBook-Pro.local>
2024-03-27 10:02:59 +0800

    修改Ex13_1課堂練習 使用函式的寫法

--- src/class_exercise/Ex13_1.java
+++ src/class_exercise/Ex13_1.java
@@ -10,50 +10,60 @@ package class_exercise;
  * • 亂數之取得可參考java.lang.Math的靜態方法random()
  * • publicstaticdoublerandom() .傳回亂數值其範圍為0.0~1.0
  * 
- * 2024/03/23
+ * 2024/03/27
  * 
  */
 
 public class Ex13_1 {
 	
+	// 陣列維度
+	final int DSIZE = 3;
+
 	public static void main(String args[]) {
 		
-		int[][] x = new int[3][3];
-		int[][] y = new int[3][3];
+		Ex13_1 testRandom = new Ex13_1();
+
+		int[][] x = testRandom.getRandomArray();
+		int[][] y = testRandom.getRandomArray();
 		int[][] z = new int[3][3];
-		
-		int dLen = 3;
-		for(int i = 0; i < dLen; i++) {
-			for(int j = 0; j < dLen; j++) {
-				x[i][j] = (int) ( Math.random() * 30 );
-				y[i][j] = (int) ( Math.random() * 30 );
+
+		int rowSize = z.length;
+		for(int i = 0; i < rowSize; i++) {
+			int colSize = z[i].length;
+			for(int j = 0; j < colSize; j++) {
 				z[i][j] = x[i][j] + y[i][j];
 			}
 		}
 		
-		for(int i = 0; i < dLen; i++) {
-			for(int j = 0; j < dLen; j++) {
-				System.out.printf("%3d", x[i][j]);
+		testRandom.printArray(x);
+		testRandom.printArray(y);
+		testRandom.printArray(z);
+	}
+	
+	public int[][] getRandomArray() {
+		int[][] data = new int[DSIZE][DSIZE];
+
+		int rowSize = data.length;
+		for(int i = 0; i < rowSize; i++) {
+			int colSize = data[i].length;
+			for(int j = 0; j < colSize; j++) {
+				data[i][j] = (int) (Math.random() * 31);
 			}
-			System.out.println("");
 		}
-		System.out.println("\n");
-		
-		for(int i = 0; i < dLen; i++) {
-			for(int j = 0; j < dLen; j++) {
-				System.out.printf("%3d", y[i][j]);
+		return data;
+	}
+
+	public void printArray(int[][] data) {
+		int rowSize = data.length;
+		for(int i = 0; i < rowSize; i++) {
+			int colSize = data[i].length;
+			for(int j = 0; j < colSize; j++) {
+				System.out.printf("%3d", data[i][j]);
 			}
-			System.out.println("");
+			System.out.println();
 		}
-		System.out.println("\n");
-		
-		for(int i = 0; i < dLen; i++) {
-			for(int j = 0; j < dLen; j++) {
-				System.out.printf("%3d", z[i][j]);				
-			}
-			System.out.println("");
-		}
-		
+
+		System.out.println();
 	}
 	
 }
